f6b0c395deaf2b57d86a71f89d62aa3203e40899
Chris Diehl <cultclassik@gmail.com>
2022-07-17 14:23:54 -0500

    update argo install

--- helm/build.sh
+++ helm/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+ns_name=argocd
 cd ..
 helm repo add argo-cd https://argoproj.github.io/argo-helm
-helm dep update charts/argo-cd/
+helm dep update helm/argo-cd/
+helm install argo-cd helm/argo-cd/ -n $ns_name --create-namespace
